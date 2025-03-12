ea4d3eb5c40479f184447f0c81374ff2a4406223
NMuellerx <niclasoz975@gmail.com>
2025-03-12 14:06:41 +0100

    Spiel dauert solange bis Gewinner feststeht

--- Würfel_des_Schicksals.py
+++ Würfel_des_Schicksals.py
@@ -11,24 +11,24 @@ def spiel_start():
             else: print("Es müssen mindestens 2 Spieler sein!")
         except ValueError:
             print("Bitte eine gültige Zahl eingeben!")
-
+    
     #Spielregeln definieren
     print("\nSpielregeln:\nJeder Spieler hat 3 Leben. Die Spieler würfeln abwechselnd,\nwobei die Augenzahl zusammenaddiert wird. Kommt ein Spieler durch würfeln über die Zahl 15,\nverliert er ein Leben und es beginnt eine neue Runde.")
     print("\nZusatzregeln: Eine gewürfelte 3 wird als O gewertet.\n")
 
     # Spieler initialisieren
     spieler = {f"Spieler {i+1}": 3 for i in range(anzahl_spieler)}
-
+    
     # Spiel starten
-    while all(l > 0 for l in spieler.values()):
+    while sum(leben > 0 for leben in spieler.values()) > 1: # Spiel läuft solange mehr als ein Spieler Leben hat
         aktuelle_summe = 0
         print("\nNeue Runde beginnt!")
-        
+
         while True:
             for name in list(spieler.keys()):
                 if spieler[name] <= 0:
                     continue # Spieler mit 0 Leben überspringen
-
+                
                 input(f"{name} ist dran. Drücke Enter zum Würfeln... ")
                 wurf = wuerfeln()
                 if wurf == 3:
@@ -38,21 +38,21 @@ def spiel_start():
                 else:
                     aktuelle_summe += wurf
                     print(f"{name} hat eine {wurf} gewürfelt. Gesamt: {aktuelle_summe}")
-
+                
                 if aktuelle_summe > 15:
-                    spieler[name] -=1
+                    spieler[name] -= 1
                     print(f"{name} hat die 15 überschritten und verliert ein Leben! Verbleibende Leben: {spieler[name]}")
+                    if spieler[name] == 0:
+                        print(f"\n{name} ist ausgeschieden. Das Spiel wird ohne ihn fortgesetzt")
                     break # Runde neu starten
-
+                
             if aktuelle_summe > 15:
                 break # Runde neu starten
-
-     # Verlierer bestimmen
-    verlierer = [name for name, leben in spieler.items()if leben == 0][0]
-    print(f"\nSpiel beendet!{verlierer} hat verloren!")
+            
+    # Gewinner bestimmen
+    gewinner = [name for name, leben in spieler.items() if leben > 0][0]
+    print(f"\nSpiel beendet! {gewinner} hat gewonnen!") 
 
 # Spiel starten
 if __name__ == "__main__":
     spiel_start()
-
-
