457d19be845c3108c7aa590bc0aebf4664797bed
NMuellerx <niclasoz975@gmail.com>
2025-03-19 13:43:46 +0100

    Add Credit,Farben,Beschreibungen; Fehler korrigier

--- Würfel_des_Schicksals.py
+++ Würfel_des_Schicksals.py
@@ -1,10 +1,27 @@
 import random 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   ITALICS = '\033[3m'
+   END = '\033[0m'
 def wuerfeln():
     return random.randint(1, 6)
 def zufallsbestrafung():
     strafen = ["ein Centershock essen", "einen albernen Tanz aufführen", "für den Rest des Spiels Sätze mit einem Grunzen beenden", "einen Zitronenschnitz essen"]
     return random.choice(strafen)
 def spiel_start(): 
+    print(color.CYAN +"╔══════════════════════════╗")
+    print("║" + color.BOLD + "  WÜRFEL DES SCHICKSALS   " + color.END + color.CYAN + "║")
+    print("║"+ color.END +"      by [Patrick Boßlet] " + color.CYAN +"║")
+    print("║"+ color.END +"      and [Niclas Müller] " + color.CYAN +"║")
+    print("╚══════════════════════════╝\n" + color.END)
     # Anzahl der Spieler abfragen
     while True:
         try: 
@@ -16,16 +33,16 @@ def spiel_start():
             print("Bitte eine gültige Zahl eingeben!")
     
     #Spielregeln definieren
-    print("\nSpielregeln:\nJeder Spieler hat 3 Leben. Die Spieler würfeln abwechselnd,\nwobei die Augenzahl zusammenaddiert wird. Kommt ein Spieler durch würfeln über die Zahl 15,\nverliert er ein Leben und es beginnt eine neue Runde.")
-    print("\nZusatzregeln: Eine gewürfelte 3 wird als O gewertet.\n")
+    print("\nSpielregeln:\nJeder Spieler hat 3 Leben. Die Spieler würfeln abwechselnd,\nwobei die Augenzahl zusammenaddiert wird. Kommt ein Spieler durch würfeln über die Zahl 15,\nverliert er ein Leben oder erhält eine Bestrafung - es folgt eine neue Runde.")
+    print("\nZusatzregeln:\n- Eine gewürfelte 3 wird als O gewertet.\n- Bei einer 6 wird nochmal gewürfelt\n- Sollte der Spieler im Begriff sein, sein letztes Leben zu verlieren,\n  hat er durch ein Gnadenbrot die Chance im Spiel zu bleiben. Hierfür muss er eine 6 würfeln")
 
     # Spieler initialisieren
     spieler = {f"Spieler {i+1}": 3 for i in range(anzahl_spieler)}
     
     # Spiel starten
-    while sum(leben > 0 for leben in spieler.values()) > 1: # Spiel läuft solange mehr als ein Spieler Leben hat
+    while sum(leben > 0 for leben in spieler.values()) > 1: # Spiel läuft, solange mehr als ein Spieler Leben hat
         aktuelle_summe = 0
-        print("\nNeue Runde beginnt!")
+        print("\nNeue Runde beginnt!\n")
 
         while True:
             for name in list(spieler.keys()):
@@ -65,9 +82,9 @@ def spiel_start():
                     else:
                         while True:
                             print(f"\n{name}, willst du...\n" ###
-                                "1 - Ein Leben verlieren?\n"  ### Abfrage ob man Leben verlieren will oder stattdessen ein Strafe in Kauf nimmt.
-                                "2 - bestraft werden?\n"      ### Geht nur wenn der Spieler noch mehr als 1 Leben hat
-                                "Deine Wahl (1 oder 2): ")    ###
+                                + color.RED + "1" + color.END + " - Ein Leben verlieren?\n"  ### Abfrage ob man Leben verlieren will oder stattdessen ein Strafe in Kauf nimmt.
+                                + color.BLUE + "2" + color.END + "- bestraft werden?\n"      ### Geht nur wenn der Spieler noch mehr als 1 Leben hat
+                                "Deine Wahl (" + color.RED + "1" + color.END + " oder " + color.BLUE + "2" + color.END+"): ")    ###
                             entscheidung = input()
                             if entscheidung == "2":
                                 print(f"{name} muss ", end="")
@@ -75,7 +92,8 @@ def spiel_start():
                                 break
                             elif entscheidung == "1":
                                 spieler[name] -= 1
-                                print(f"{name} verliert ein Leben. Verbleibende Leben: {spieler[name]}")
+                                print(f"{name} verliert ein Leben. " + color.ITALICS + "Verbleibende Leben: ", end="")
+                                print(color.GREEN + f"{spieler[name]}" + color.END)
                                 break
                             else:
                                 print("Bitte 1 oder 2 eingeben.") # Aufforderung zur korrekten Eingabe nach falscher Eingabe
@@ -86,7 +104,8 @@ def spiel_start():
 
     # Gewinner bestimmen
     gewinner = [name for name, leben in spieler.items() if leben > 0][0]
-    print(f"\nSpiel beendet! {gewinner} hat gewonnen!") 
+    print("\nSpiel beendet! ", end="")
+    print(color.YELLOW + f"{gewinner} hat gewonnen!" + color.END) 
 
 # Spiel starten
 if __name__ == "__main__":
